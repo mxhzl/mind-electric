88c2de35fd1956bc0a37fef84caa0de541085b48
Maxine <maxine@mxhzl.com>
2025-05-07 22:38:57 -0400

    use postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_05_06_042636) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "logs", force: :cascade do |t|
     t.date "date"
     t.string "trigger"

57822a9ba2c61f09d5aea3afead8f9c4773d4a98
Maxine <krebs.maxine@gmail.com>
2025-09-26 15:19:15 -0400

    Views and controller for forms CRUD

---
+++ app/controllers/forms_controller.rb
@@ -0,0 +1,51 @@
+class FormsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_form, only: %i[ show edit update destroy ]
+
+  def index
+    @forms = Form.all
+  end
+
+  def show
+  end
+
+  def new
+    @form = Form.new
+  end
+
+  def create
+    @form = Form.new(form_params)
+    if @form.save
+      redirect_to @form, notice: "Form Created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @form.update(form_params)
+      redirect_to @form, notice: "Form Updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @form.destroy
+    redirect_to forms_path, notice: "Form Deleted!"
+  end
+
+  private
+
+  def set_form
+    @form = Form.find_by(id: params[:id])
+    redirect_to root_path,  warning: "can't find log with that id" if @form.nil?
+  end
+
+  def form_params
+    params.expect(form: [ :name, :active ])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  resources :logs
+  resources :logs, :forms, :fields, :responses
 end
